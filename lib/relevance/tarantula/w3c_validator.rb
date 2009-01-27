fdf9447cbd651f84922c49a9810526b14359f035
Roman Shterenzon <roman.shterenzon@gmail.com>
2009-01-27 10:28:17 +0200

    Make w3c validation handler depend on the bundled w3c validators.

--- lib/relevance/tarantula/w3c_validator.rb
+++ lib/relevance/tarantula/w3c_validator.rb
@@ -1,32 +1,26 @@
-require 'rubygems'
-begin
-  require 'w3c_validators'
-rescue LoadError
-  $stderr.puts 'w3c_validators gem is not available'
-end
+# Depends on a bundled version of w3c_validators
+require "#{File.dirname(__FILE__)}/../../../vendor/w3c_validators/lib/w3c_validators"
 
-if defined? W3CValidators
-  class Relevance::Tarantula::W3CValidator
-    include Relevance::Tarantula
+class Relevance::Tarantula::W3CValidator
+  include Relevance::Tarantula
 
-    # Possible options (passed through to MarkupValidator):
-    # :validator_uri => 'http://localhost/w3c-validator/check'
-    def initialize(options = {})
-      @validator = W3CValidators::MarkupValidator.new(options)
-    end
+  # Possible options (passed through to MarkupValidator):
+  # :validator_uri => 'http://localhost/w3c-validator/check'
+  def initialize(options = {})
+    @validator = W3CValidators::MarkupValidator.new(options)
+  end
 
-    def handle(result)
-      response = result.response
-      return unless response.html? and response.code == '200'
+  def handle(result)
+    response = result.response
+    return unless response.html? and response.code == '200'
 
-      results = @validator.validate_text(response.body)
+    results = @validator.validate_text(response.body)
 
-      if results.errors.length > 0
-        error_result = result.dup
-        error_result.description = "Bad HTML (W3C Validator)"
-        error_result.data = results.errors.collect {|e| "Line: #{e.line}, column: #{e.col}, error: #{e.message}"}.join("\n")
-        error_result
-      end
+    if results.errors.length > 0
+      error_result = result.dup
+      error_result.description = "Bad HTML (W3C Validator)"
+      error_result.data = results.errors.collect {|e| "Line: #{e.line}, column: #{e.col}, error: #{e.message}"}.join("\n")
+      error_result
     end
   end
 end
